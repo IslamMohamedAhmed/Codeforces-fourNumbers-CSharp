5c4890f4f88040ae62357e191aff548dd5225b10
unknown <islammohamedahmed49@gmail.com>
2024-03-09 13:50:10 +0200

    four numbers

---
+++ FourNumbers/Program.cs
@@ -0,0 +1,11 @@
+﻿long a, b, c, d;
+string str  = Console.ReadLine();
+var arr = str.Split(" ");
+a = Convert.ToInt64(arr[0]);
+b = Convert.ToInt64(arr[1]);
+c = Convert.ToInt64(arr[2]);
+d = Convert.ToInt64(arr[3]);
+
+long res = a * b * c * d;
+
+Console.WriteLine(res);
